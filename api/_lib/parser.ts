3715a709209b0a4b6d7de05d29756d99396704af
Gabe <gabrielloubier1@gmail.com>
2021-07-22 16:58:58 -0400

    *

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -56,8 +56,8 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 function getDefaultImages(images: string[], theme: Theme): string[] {
   const defaultImage =
     theme === "light"
-      ? "https://builtoncardano.com/static/images/og.png"
-      : "https://builtoncardano.com/static/images/logo.svg";
+      ? "https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg"
+      : "https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg";
 
   if (!images || !images[0]) {
     return [defaultImage];
